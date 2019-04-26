5b2530a72c6262171d8606e3f12e32ffc0b8cfa8
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-04-26 00:34:31 -0400

    yelp api call based on curr user; backend
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- server/routes/api/schedule.js
+++ server/routes/api/schedule.js
@@ -17,17 +17,52 @@ const axios = require('axios');
 //     }
 // }
 
+function yelp(res, prefs) {
+      const preferences = prefs[0];
+      const food = preferences.preferences[0].food;
+      // const activities = preferences.preferences[0].activities;
+      const location = preferences.preferences[0].location;
+      const date = preferences.preferences[0].date;
+      const radius = preferences.preferences[0].radius;
+
+      const yelp = require('yelp-fusion');
+      const apiKey = require('../../../config/api').YELP_KEY;
+      console.log(food);
+      const searchRequest = {
+        location: location,
+        radius: Number(radius),
+        categories: food,
+        limit: 10,
+      };
+      const client = yelp.client(apiKey);
+
+      client.search(searchRequest).then(response => {
+        const firstResult = response.jsonBody.businesses[0];
+        const prettyJson = JSON.stringify(firstResult, null, 4);
+        // const prettyJson = JSON.stringify(response.jsonBody, null, 4);
+        // console.log('hi');
+        console.log(response.jsonBody.businesses[0]);
+        console.log(response.jsonBody);
+        // console.log(prettyJson);
+        res.send(
+          response.jsonBody.businesses,
+        );
+      }).catch(e => {
+        console.log(e);
+    });
+}
+
 router.get('/', (req, res) => {
-    async function getUser() {
-      try {
-          const response = await auth.get('/auth/user');
-        console.log(response);
-      } catch (error) {
-        console.error(error);
-      }
+    if (req.user) {
+        Pref.find({user_id:req.user._id})
+            .sort({ date: -1 })
+            .then(items => 
+                yelp(res, items)
+            );
+    } else {
+        return res.json({ user: null })
     }
-    getUser();
- });
+});
 
 // @route   GET api/schedule
 // @desc    Get All Schedules
