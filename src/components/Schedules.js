753d00295f1b92ca6e2b3b6c55c4ef58a11abb38
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-04-26 13:26:30 -0400

    outputting names of restaurants onto page for schedule
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- src/components/Schedules.js
+++ src/components/Schedules.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import { JsonToTable } from "react-json-to-table";
 
 // Styling
 import '../App.css';
@@ -10,34 +11,42 @@ class Schedules extends Component {
 	constructor() {
 		super();
 		this.state = {
-		    businesses: "", 
+		    businesses: [], 
 		    redirectTo: null
 		};
-        this.getSchedules = this.getSchedules.bind(this);
+	    this.printSchedules = this.printSchedules.bind(this);
 	}
-    getSchedules () {
-        axios
+    componentDidMount() {
+        this.getSchedules();
+    }
+    getSchedules = async () => {
+        await axios
             .get('/api/schedules')
             .then(response => {
              let businesses = JSON.parse(response.request.response).map((t) => [{rating:t.rating, name:t.name, location:t.location}])
-            console.log(businesses);
             this.setState({
-                businesses: businesses
+                businesses: Array.from(businesses)
             });
-            var business = businesses.map((e) => 
-            <div>
-               e.name 
-           </div>
-            );
-            return (<div>{business}</div>)
+            console.log(this.state.businesses);
         })
+    };
+
+    printSchedules() {
+        let keys = Object.keys(this.state.businesses)
+        return ( keys.map((i) => this.state.businesses[i].map(business => <div>{business.name}</div>)) )
     }
+
 	render(props) {
 		if (this.state.redirectTo) {
 			return <Redirect to={{ pathname: this.state.redirectTo }} />
 		}
         else {
-            this.getSchedules()
+            return(
+                <div id="businesses">
+                    {this.printSchedules()}
+                    
+                </div>
+            )
         }
 	}
 }
