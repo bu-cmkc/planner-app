a931a01de49201bf45f87f9a45a15bc1b599f4d2
Masayoshi Iwasa <miwasa20@bu.edu>
2019-04-25 21:18:32 -0400

    navbar edited

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Route, Link } from 'react-router-dom'
 
 
 // import logo from './logo.svg';
+// import Anpm install --save bootstrappp.css and bootstrap css
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import LoginForm from './components/Login/LoginForm';
 import SignupForm from './components/SignupForm';
@@ -16,54 +18,78 @@ import YelpFusion from './components/YelpFusion';
 const DisplayLinks = props => {
 	if (props.loggedIn) {
 		return (
-			<nav className="navbar" bg="light" expand="lg">
-				<ul className="nav">
-					<li className="nav-item">
-						{/* <Link to="/" className="nav-link">
-							<img src={logo} style={{width:100, marginTop: -7}} />
-							Home
-						</Link> */}
-						<a href="#" className='navbar-brand'><img src="https://www.w3schools.com/images/w3schools_green.jpg"/></a>
-					</li>
-					<li className="nav-item">
-                        <Link to="/addPreferences" className="nav-link">
-							AddPreferences
-						</Link>
-					</li>
-					<li className="nav-item">
-                        <Link to="/yelpFusion" className="nav-link">
-							Yelp Fusion
-						</Link>
-					</li>
-					<li>
-						<Link to="#" className="nav-link" onClick={props._logout}>
-							Logout
-						</Link>
-					</li>
-				</ul>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  				<a href="#" className='navbar-logo'><img src={require("./SP_logo.png")}/></a>
+  				{/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  			  		<span class="navbar-toggler-icon"></span>
+  				</button> */}
+  				<div class="collapse navbar-collapse" id="navbarText">
+  			  		<ul class="navbar-nav mr-auto">
+  			    		<li class="nav-item">
+							<Link to="/addPreferences" className="nav-link">
+			 					Preferences
+			 				</Link>
+  			    		</li>
+  			    		<li class="nav-item">
+							<Link to="/yelpFusion" className="nav-link">
+								Yelp Fusion
+				 			</Link>
+  			    		</li>
+  			    		<li class="nav-item">
+						  	<Link to="#" className="nav-link" onClick={props._logout}>
+			 					Logout
+				 			</Link>
+  			    		</li>
+  			  		</ul>
+  			  		<span class="navbar-logo">
+						<img src={require("./SP_logo.png")}/>
+  			  		</span>
+  				</div>
 			</nav>
-			
 		)
 	} else {
 		return (
-			<nav className="navbar">
-				<ul className="nav">
-					<li className="nav-item">
-						<Link to="/" className="nav-link">
-							Home
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link to="/login" className="nav-link">
-							login
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link to="/signup" className="nav-link">
-							sign up
-						</Link>
-					</li>
-				</ul>
+			// <nav className="navbar">
+			// 	<ul className="nav">
+			// 		<li className="nav-item">
+			// 			<Link to="/" className="nav-link">
+			// 				Home
+			// 			</Link>
+			// 		</li>
+			// 		<li className="nav-item">
+			// 			<Link to="/login" className="nav-link">
+			// 				login
+			// 			</Link>
+			// 		</li>
+			// 		<li className="nav-item">
+			// 			<Link to="/signup" className="nav-link">
+			// 				sign up
+			// 			</Link>
+			// 		</li>
+			// 	</ul>
+			// </nav>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  				<a href="#" className='navbar-logo'><img src={require("./SP_logo.png")}/></a>
+  				{/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  			  		<span class="navbar-toggler-icon"></span>
+  				</button> */}
+  				<div class="collapse navbar-collapse" id="navbarText">
+  			  		<ul class="navbar-nav mr-auto">
+  			    		<li class="nav-item">
+						  	<Link to="/login" className="nav-link">
+				 				login
+				 			</Link>
+  			    		</li>
+  			    		<li class="nav-item">
+							<Link to="/signup" className="nav-link">
+								sign up
+				 			</Link>
+  			    		</li>
+  			  		</ul>
+  			  		<span class="navbar-logo">
+						<img src={require("./SP_logo.png")}/>
+  			  		</span>
+  				</div>
 			</nav>
 		)
 	}
@@ -198,10 +224,10 @@ class App extends Component {
 		if (this.state.user === null) {
 			return (
 				<div className="App">
-					<h1>This is the main App component</h1>
-					<p>hello guest</p>
 					{/* LINKS to our different 'pages' */}
 					<DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} />
+					<h1>Welcome to Smart Planner</h1>
+					<p>Please login if you own an account. Sign up using email or google account if you're a new member!</p>
 					{/*  ROUTES */}
 					{/* <Route exact path="/" component={Home} /> */}
 					<Route exact path="/" render={() => <Home user={this.state.user} />} />
@@ -223,7 +249,7 @@ class App extends Component {
 				<div className="App">
 					{/* LINKS to our different 'pages' */}
 					<DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} />
-					<h1>Planner App</h1>
+					<h1>Smart Planner</h1>
 					
 					{/*  ROUTES */}
 					{/* <Route exact path="/" component={Home} /> */}
