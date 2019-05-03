1308838208d4d255c1ef2b440cadfaa613008145
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-05-03 18:07:02 -0400

    added example config file
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

---
+++ config/api_example.js
@@ -0,0 +1,19 @@
+// Example config file
+// Change the name of this file to api.js
+
+const YELP_KEY = '';
+
+const Google = {
+    CAL_KEY : '', 
+    CLIENT_ID: '',
+    CLIENT_SECRET: ''
+    
+}
+
+const EVENTBRITE_KEY = '';
+
+module.exports = {
+    YELP_KEY,
+    Google, 
+    EVENTBRITE_KEY
+}
